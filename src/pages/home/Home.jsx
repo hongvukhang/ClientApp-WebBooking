cc95c6a3563c01ded8b1954e62b7e46a322588fc
Khang2501 <105971812+Khang2501@users.noreply.github.com>
2024-03-03 23:24:24 +0700

    update assginment

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,30 +6,30 @@ import MailList from "../../components/mailList/MailList";
 import Navbar from "../../components/navbar/Navbar";
 import PropertyList from "../../components/propertyList/PropertyList";
 import "./home.css";
-import axios from 'axios'
-import { useEffect , useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 const Home = () => {
-
-  const [hotel, setHotel] = useState({area:{}, type:{}, rating:[]})
-  useEffect(()=>{
-    axios.get('/hotel')
-    .then(result=>setHotel(result.data))
-    .catch(err=>console.log(err))
-  },[])
+  const [hotel, setHotel] = useState({ area: {}, type: {}, rating: [] });
+  useEffect(() => {
+    axios
+      .get("/hotel")
+      .then((result) => setHotel(result.data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
-    <div>
+    <>
       <Navbar />
-      <Header/>
+      <Header />
       <div className="homeContainer">
-        <Featured area={hotel.area}/>
+        <Featured area={hotel.area} />
         <h1 className="homeTitle">Browse by property type</h1>
-        <PropertyList type={hotel.type}/>
+        <PropertyList type={hotel.type} />
         <h1 className="homeTitle">Homes guests love</h1>
-        <FeaturedProperties rating={hotel.rating}/>
-        <MailList/>
-        <Footer/>
+        <FeaturedProperties rating={hotel.rating} />
+        <MailList />
+        <Footer />
       </div>
-    </div>
+    </>
   );
 };
 
